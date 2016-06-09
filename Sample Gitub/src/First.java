37dd5aa624ede93b5a01d8c7d51662d18695ce0e
ajayoct31@gmail.com <ajjuoct31>
2016-06-09 11:03:50 +0530

    V0.0.1
    First Release

---
+++ Sample Gitub/src/First.java
@@ -0,0 +1,18 @@
+
+public class First {
+
+	static int a=10;
+	
+	static int b=10;
+	
+	static int c;
+	public static void main(String[] args) {
+		
+		System.out.println("Pass");
+		// TODO Auto-generated method stub
+
+		c= a*b;
+		System.out.println("Mulytiplication is" +c);
+	}
+
+}
